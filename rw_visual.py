52718535fd90233e3210d64789810c2411d896bd
lukaiyi <lukaiyi@qq.com>
2021-01-04 16:05:38 +0800

    模拟随机漫步

--- rw_visual.py
+++ rw_visual.py
@@ -2,7 +2,13 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# 程序处于活动状态时，就不断模拟随机漫步
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make a another walk? (y/n): ")
+    if keep_running == 'n':
+        break
