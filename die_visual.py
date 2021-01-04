c23d1b6bdd25e892127b360b4e4ce9a1586e0f55
lukaiyi <lukaiyi@qq.com>
2021-01-04 17:11:56 +0800

    分析结果

--- die_visual.py
+++ die_visual.py
@@ -5,7 +5,14 @@ die = Die()
 
 # 投掷几次骰子并将结果存储在列表当中
 results = []
-for roll_num in range(100):
-    results.append(die.roll())
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
 
-print(results)
+# print(results)
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
