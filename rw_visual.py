b6a03304f72aab890b08c197ebcbe384421c7b94
lukaiyi <lukaiyi@qq.com>
2021-01-04 16:09:53 +0800

    给点着色

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,9 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolors='none', s=15)
     plt.show()
 
     keep_running = input("Make a another walk? (y/n): ")
