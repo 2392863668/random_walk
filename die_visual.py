57ac8e5f8b92ffad0eb4a8dc18f9cae2fbfb70e6
lukaiyi <lukaiyi@qq.com>
2021-01-07 16:39:05 +0800

    同时投掷两个骰子

--- die_visual.py
+++ die_visual.py
@@ -2,24 +2,26 @@ import pygal
 from die import Die
 
 # 创建一个 D6
-die = Die()
+die1 = Die()
+die2 = Die()
 
 # 投掷几次骰子并将结果存储在列表当中
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die1.roll() + die2.roll()
     results.append(result)
 
 # print(results)
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # print(frequencies)
 # 对结果进行可视化
 hist = pygal.Bar()
-hist.title = "Results of rolling one D6 1000 times."
+hist.title = "Results of rolling two D6 1000 times."
 hist.x_labels = ['1', '2', '3', '4', '5', '6']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
