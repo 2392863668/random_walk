8b3e84e857d1dcadd2bfc3ef3d4ec81f1b6312bb
lukaiyi <lukaiyi@qq.com>
2021-01-12 16:28:26 +0800

    设置地图显示背景

--- world_population.py
+++ world_population.py
@@ -9,6 +9,7 @@
 import json
 
 import pygal
+from pygal.style import RotateStyle, LightColorizedStyle
 
 from countries import get_country_code
 
@@ -40,7 +41,8 @@ for cc, pop in cc_populations.items():
 # 查看每个组分别包含多少个国家
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm = pygal.maps.world.World()
+wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = "World Population in 2010, by Country"
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
